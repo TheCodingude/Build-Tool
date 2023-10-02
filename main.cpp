328778d98f029e5f03a65e6a0e1f19eec8d61dfb
Codingdude <sharpdt06@gmail.com>
2023-10-01 21:38:16 -0400

    Started Variables Stuff

--- main.cpp
+++ main.cpp
@@ -2,26 +2,18 @@
 #include <fstream>
 #include <string>
 #include <string.h>
+#include <map>
+
 
 using namespace std;
 
+map<string, string> variables;
 
 void execute_command(char* command){
     if(system(command) != 0){
         cerr << "Unable to run command: " << command << endl; 
     } 
 }
-
-string convertToString(char* a, int size)
-{
-    int i;
-    string s = "";
-    for (i = 0; i <= size; i++) {
-        s = s + a[i];
-    }
-    return s;
-}
-
 
 string CHECK_OS(){
     #ifdef _WIN32
@@ -37,7 +29,6 @@ string removeNewlines(char* line) {
     int size = sizeof(line);
     char newstr[size];
     int index = 0;
-    int smth = 0;
 
     while(index <= size){
         if (line[index] == '\n' || line[index] == '\r' || line[index] == ' '){
@@ -50,10 +41,14 @@ string removeNewlines(char* line) {
         
     }
 
-    string line_a = convertToString(newstr, sizeof(newstr));
+    return newstr;
 
-    return line_a;
+}
 
+
+char find_var(string line, int location){
+    cout << line << endl;
+    cout << location << endl;
 }
 
 
@@ -67,6 +62,7 @@ void read_file(){
         exit(1);
     }
 
+    bool flags_exist = false;
     string os = CHECK_OS();
     string os_tag = "none";
 
@@ -80,13 +76,14 @@ void read_file(){
         string line = removeNewlines(buffer);
         
         if(line.substr(0, 2) == "//"){
-            continue;
+            continue;   
         }
 
         string winline = line.substr(0, 9);
         string linuxline = line.substr(0, 7);
         string macline = line.substr(0, 5);
-
+        line = (string) buffer;
+        int equal = line.find("=");
 
         if (winline == "[WINDOWS]"){ // CHECK IF CURRENTLY IN OS SECTION AND WHICH OS IT IS
             os_tag = "windows";
@@ -99,6 +96,10 @@ void read_file(){
         }   
         else if(macline == "[END]"){ // this was is also mac line just because they're the same length lol
             os_tag = "none";
+        }
+        else if(equal <= sizeof(line) && equal >= 0){
+            cout << '?' << endl;
+            find_var(line, equal);
         }
         else if (os_tag != "none"){
             if(os_tag == os){
@@ -116,7 +117,6 @@ void read_file(){
 
 
 
-
         
     }
 
@@ -128,7 +128,10 @@ void read_file(){
 
 
 
-int main(){
+int main(int argc, char** argv){
+
+    (void) argc;
+    (void) argv;
     read_file();
     return 0;
 }
